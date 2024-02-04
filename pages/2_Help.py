7b0bdc82863447938c0f3846febe8956aa136d42
thedirtypeach <tomgag2000@gmail.com>
2024-02-04 03:27:18 -0500

    Initial commit

---
+++ pages/2_Help.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Meeeeeeeeeeeep!")
+st.text("hello, Celia and Ryder are sleeping next door.")
+st.text("I can make changes to my web app on the fly with streamlit.")
